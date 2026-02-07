25e42e848dd50452f71eabf08552197c815f6654
romainD <romain.deschamps61@sfr.fr>
2026-02-07 13:38:59 +0100

    Update TLD 2.51

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@
 [assembly: AssemblyProduct("BedrollTweaker")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.1.0")]
-[assembly: MelonInfo(typeof(BedrollTweaker.Main), "BedrollTweaker", "2.1.0", "Romain, GruffCassquatch, Phaedrus, The Illusion")]
+[assembly: AssemblyVersion("2.1.1")]
+[assembly: AssemblyFileVersion("2.1.1")]
+[assembly: MelonInfo(typeof(BedrollTweaker.Main), "BedrollTweaker", "2.1.1", "Romain, GruffCassquatch, Phaedrus, The Illusion")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
